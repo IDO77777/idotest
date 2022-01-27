ca8d73938efe7877809ce9abe99c806ac7af61b0
IDO77777 <98142890+IDO77777@users.noreply.github.com>
2022-01-27 11:28:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let icon =
+  '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
+
+miro.onReady(() => {
+  miro.initialize({
+    extensionPoints: {
+      toolbar: {
+        title: '物と情報の流れ図',
+        librarySvgIcon: icon24,
+        toolbarSvgIcon: icon24,
+        onClick: () => {
+          miro.board.ui.openLibrary('sidebar.html', {title: '物と情報の流れ図'})
+        }, 
+    },
+  })
+})
