a592ef5e3abb086b1f7599a484a69381113a8b14
IDO77777 <98142890+IDO77777@users.noreply.github.com>
2022-01-28 09:34:58 +0900

    Update content.js

--- content.js
+++ content.js
@@ -162,7 +162,7 @@ function bootstrap() {
       return {
         //url: `data:image/svg+xml,%3Csvg width='140' height='140' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Crect stroke='null' x='0' y='0' fill='%23${currentShapeColor}' height='140' width='140'/%3E%3C/g%3E%3C/svg%3E`,
         createShape(canvasX, canvasY, currentShapeColor, currentShapeText, currentShapeType, currentShapeOpacity)
-      }
+      },
     },
     onDrop: (canvasX, canvasY) => {
       console.log('onDrop 2')
